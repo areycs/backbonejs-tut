baa9d6347f77606500138fee0003f300e11f8728
Alexander Reynolds <wintermute.infinite@gmail.com>
2017-03-09 10:45:41 -0700

    initial add of completed project

---
+++ js/app.js
@@ -0,0 +1,15 @@
+Organizer = {
+	initialize: function() {
+		Organizer.events = new Organizer.EventsCollection();
+		Organizer.appLayout = new Organizer.BaseAppLayout({
+			el: '#app'
+		});
+
+		Organizer.router = new Organizer.Router();
+		Backbone.history.start();
+	}
+};
+
+$(document).ready(function() {
+	Organizer.initialize();
+});
